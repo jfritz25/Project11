750e8b84ad61e4943ee758cd0ef5cd6520253538
hbahramian <habahram@iu.edu>
2023-12-02 23:42:48 -0500

    .

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,8 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { SplashScreen, Stack, useRouter } from 'expo-router';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -43,14 +42,21 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
+  const router = useRouter();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+       <Stack.Screen name="(modals)/login" options={{
+        title: 'Log in or sign up',
+        presentation: 'modal',
+        headerRight: () => (
+          <TouchableOpacity onPress={() => router.back()}>
+             <FontAwesome name="close" size={24} color="black" />
+          </TouchableOpacity>
+        )
+       }} />
       </Stack>
-    </ThemeProvider>
+   
   );
 }
